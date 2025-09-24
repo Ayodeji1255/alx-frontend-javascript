84b8f0c401b6b8b966b179da20ddfd4f2f26bc01
ayodeji1255 <xflimzy4ever@gmail.com>
2025-09-24 22:07:28 +0100

    bruv

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -3,42 +3,97 @@ interface DirectorInterface {
   getCoffeeBreak(): string;
   workDirectorTasks(): string;
 }
-
 interface TeacherInterface {
   workFromHome(): string;
   getCoffeeBreak(): string;
   workTeacherTasks(): string;
 }
 class Director implements DirectorInterface {
+  salary: number;
+
+  constructor(salary: number) {
+    this.salary = salary;
+  }
+
   workFromHome(): string {
-    return 'Working from home';
+    return "Working from home";
   }
   getCoffeeBreak(): string {
-    return 'Getting a coffee break';
+    return "Getting a coffee break";
   }
   workDirectorTasks(): string {
-    return 'Getting to director tasks';
+    return "Getting to director tasks";
   }
 }
 
 class Teacher implements TeacherInterface {
+  salary: number;
+
+  constructor(salary: number) {
+    this.salary = salary;
+  }
+
   workFromHome(): string {
-    return 'Cannot work from home';
+    return "Cannot work from home";
   }
   getCoffeeBreak(): string {
-    return 'Cannot have a break';
+    return "Cannot have a break";
   }
   workTeacherTasks(): string {
-    return 'Getting to work';
+    return "Getting to work";
   }
-} 
-function createEmployee(salary: number | string): Director | Teacher {
-  if (typeof salary === 'number' && salary < 500) {
-    return new Teacher();
-  }
-  return new Director();
 }
 
+const teacherSalary = new Teacher(200);
+const directorSalary = new Director(500);
+
+let convertToNumber: number;
+const dirctorAverageSalary = 500;
+
+function createEmployee(salary: number | string) {
+  let numericSalary: number;
+  if (typeof salary === "string") {
+    numericSalary = parseInt(salary.replace(/[^0-9]/g, ""), 10);
+  } else {
+    numericSalary = salary;
+  }
+  if (numericSalary < 500) {
+    return new Teacher(numericSalary);
+  } else {
+    return new Director(numericSalary);
+  }
+}
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === "Math") console.log("Teaching Math");
+  if (todayClass === "History") console.log("Teaching History");
+}
+
+const emp1 = createEmployee("$200");
+const emp2 = createEmployee(500);
+const emp3 = createEmployee(100);
+
+
+console.log(createEmployee("$500"));
 console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+console.log(createEmployee(200));
+
+console.log(executeWork(createEmployee(100)));
+console.log(executeWork(createEmployee(2000)));
+console.log(executeWork(createEmployee(300)));
+
+teachClass("Math");
+teachClass("History");

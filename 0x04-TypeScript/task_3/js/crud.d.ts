e56016235eeb83fd3b075f17fe4b4ceecdbacaaf
ayodeji1255 <xflimzy4ever@gmail.com>
2025-09-24 21:44:44 +0100

    crudts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -3,21 +3,21 @@
 import { RowID, RowElement } from './interface';
 
 export declare module 'crud' {
-  export function insertRow(row: RowElement): RowID;
-  export function deleteRow(rowId: RowID): void;
-  export function updateRow(rowId: RowID, row: RowElement): RowID;
+  export declare function insertRow(row: RowElement): RowID;
+  export declare function deleteRow(rowId: RowID): void;
+  export declare function updateRow(rowId: RowID, row: RowElement): RowID;
 }
 
-export function insertRow(row: RowElement): number {
+export declare function insertRow(row: RowElement): number {
   throw new Error('Function not implemented.');
 }
 
 
-export function updateRow(newRowID: number, updatedRow: RowElement) {
+export declare function updateRow(newRowID: number, updatedRow: RowElement) {
   throw new Error('Function not implemented.');
 }
 
 
-export function deleteRow(newRowID: number) {
+export declare function deleteRow(newRowID: number) {
   throw new Error('Function not implemented.');
 }

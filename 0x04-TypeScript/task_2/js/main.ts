490b9d037dcc671323888dad0b4a9949ae07723f
ayodeji1255 <xflimzy4ever@gmail.com>
2025-09-24 22:13:11 +0100

    fly

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -77,7 +77,7 @@ function executeWork(employee: Director | Teacher): string {
 
 type Subjects = "Math" | "History";
 
-function teachClass(todayClass:Subjects) {
+function teachClass([todayClass: Subjects]) {
   if (todayClass === "Math") console.log("Teaching Math");
   if (todayClass === "History") console.log("Teaching History");
 }
